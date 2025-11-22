ded31257be915d60b7fcd3a4f9f87ee79c6964d3
Shekinah Adeogo <adeogoshekinah@gmail.com>
2025-11-22 13:43:48 +0100

    feat: set up routing and layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import MainLayout from './layout/MainLayout';
+import Invoices from './pages/Invoices';
+import Clients from './pages/Clients';
+import ServiceItems from './pages/ServiceItems';
+
 function App() {
-  return <h1>Vite + React</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<MainLayout />}>
+          <Route index element={<Invoices />} />
+          <Route path='clients' element={<Clients />} />
+          <Route path='service-items' element={<ServiceItems />} />
+          <Route path='*' element={<Navigate to='/' replace />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from 'react-router-dom';
+import Navbar from '../components/Navbar';
+
+const MainLayout = () => {
+  return (
+    <div className='w-screen min-h-screen bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-gray-100'>
+      <Navbar />
+      <main className='mx-auto max-w-6xl px-4 sm:px-6 lg:px-8 py-8'>
+        <Outlet />
+      </main>
+    </div>
+  );
+};
+
+export default MainLayout;
